f81971a0e11baedde8b4ee0bcc4267d59c87c3f7
kalkidan-solomon-coder <152845891+kalkidan-solomon-coder@users.noreply.github.com>
2024-04-17 10:27:22 +0300

    first commit

---
+++ home-page/src/Navbar.jsx
@@ -0,0 +1,30 @@
+function Navbar(){
+return(
+    <>
+    <div className="container">
+<div>
+    <img src="./japanlogo.webp"/>
+</div>
+<div className="nav-2-container">
+    <div>
+        Jobs
+    </div>
+    <div>
+        Companies
+    </div>
+    <div>
+        Blog
+    </div>
+    <div>
+        Log In
+    </div>
+    <div>
+        <button>Register</button>
+    </div>
+    <div>Employers</div>
+</div>
+    </div>
+    </>
+)
+}
+export default Navbar
